1713065626a11130f0614f9a8dbefe0e7f87eb8e
Roey Benamotz <roeybenamotz@Roeys-MacBook-Pro.local>
2019-04-14 19:56:38 -0400

    MQTT improvements

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -14,42 +14,6 @@ PubSubClient client(espClient);
 bool lastPublishedState = false;
 bool lastPublishedSensorState = false;
 
-void callback(char *topic, byte *payload, unsigned int length)
-{
-  if (length <= 0)
-  {
-    writeToLog("Toggling lights due to MQTT command");
-    lightingToggle();
-    return;
-  }
-  if (((char *)payload)[1] == 'N')
-  {
-    writeToLog("Turning lights ON due to MQTT command");
-    lightingOn();
-  }
-  else
-  {
-    writeToLog("Turning lights OFF due to MQTT command");
-    lightingOff();
-  }
-}
-
-bool reconnectToMqtt()
-{
-  bool output = client.connect(HOST_NAME, MQTT_USER, MQTT_PASS);
-  if (output)
-  {
-    client.subscribe(MQTT_TOPIC_IN);
-  }
-  return output;
-}
-
-void setupMqtt()
-{
-  client.setServer(MQTT_HOST_NAME, MQTT_PORT);
-  client.setCallback(callback);
-}
-
 void publishLightingState()
 {
   String state = "OFF";
@@ -60,10 +24,57 @@ void publishLightingState()
 #ifdef ENV_DEV
   writeToLog("Fake publiushing %s to %s", state.c_str(), MQTT_TOPIC_OUT);
 #else
-   client.publish(MQTT_TOPIC_OUT, state.c_str());
+  client.publish(MQTT_TOPIC_OUT, state.c_str());
 #endif
- 
+
   lastPublishedState = isLightsOn;
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  if (length <= 0)
+  {
+    writeToLog("Toggling lights due to MQTT command");
+    lightingToggle();
+    publishLightingState();
+    return;
+  }
+  if (((char *)payload)[1] == 'N')
+  {
+    writeToLog("Turning lights ON due to MQTT command");
+    lightingOn();
+    publishLightingState();
+  }
+  else
+  {
+    writeToLog("Turning lights OFF due to MQTT command");
+    lightingOff();
+    publishLightingState();
+  }
+}
+
+void reconnectToMqtt()
+{
+  if (client.state() == MQTT_CONNECTED)
+  {
+    return;
+  }
+  bool output = client.connect(HOST_NAME, MQTT_USER, MQTT_PASS);
+  if (output)
+  {
+    writeToLog("Connected to MQTT %s as %s", HOST_NAME, MQTT_USER);
+    client.subscribe(MQTT_TOPIC_IN);
+  }
+  else
+  {
+    writeToLog("Failed to connected to MQTT %s as %s", HOST_NAME, MQTT_USER);
+  }
+}
+
+void setupMqtt()
+{
+  client.setServer(MQTT_HOST_NAME, MQTT_PORT);
+  client.setCallback(callback);
 }
 
 void publishSensorState()
@@ -87,16 +98,21 @@ void publishSensorState()
 
 void loopMqtt()
 {
+  static unsigned long lastLoop = 0;
   static unsigned long lastTimeLightingStatePublished = 0;
-  if (!client.loop())
+  if (!globalIsWifiConnected)
   {
-    bool b = reconnectToMqtt();
-    if (!b)
-    {
-      return;
-    }
+    return;
   }
-  unsigned long l = millis() - lastTimeLightingStatePublished;
+  unsigned long l = millis() - lastLoop;
+  if (l < 10)
+  {
+    return;
+  }
+  reconnectToMqtt();
+  client.loop();
+  lastLoop = millis();
+  l = millis() - lastTimeLightingStatePublished;
   if (l > 5000)
   {
     publishLightingState();
